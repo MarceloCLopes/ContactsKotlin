dcded1e9b56b590420744d30e0a7c5a5a74f4596
MarceloCLopes <marc01mcl@gmail.com>
2021-03-17 16:55:50 -0300

    created methods in main activity

--- app/src/main/java/com/mcl/contacts/MainActivity.kt
+++ app/src/main/java/com/mcl/contacts/MainActivity.kt
@@ -2,10 +2,44 @@ package com.mcl.contacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindView()
+        updateList()
     }
+
+    private fun bindView(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Marcelo Lopes",
+                    "(00) 0000-00000",
+                    "img.png"
+                ),
+                Contact(
+                    "Marcelo Claro",
+                    "(99) 9999-99999",
+                    "img.png"
+            )
+            )
+        )
+    }
+
 }
