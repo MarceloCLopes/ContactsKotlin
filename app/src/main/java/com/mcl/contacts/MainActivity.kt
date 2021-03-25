6b69c93f3c74a133858b2c8dc4cc2620881ead3d
MarceloCLopes <marc01mcl@gmail.com>
2021-03-25 16:45:21 -0300

    updated main activity class

--- app/src/main/java/com/mcl/contacts/MainActivity.kt
+++ app/src/main/java/com/mcl/contacts/MainActivity.kt
@@ -1,50 +1,90 @@
 package com.mcl.contacts
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.Message
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.edit
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.mcl.contacts.DetailActivity.Companion.EXTRA_CONTACT
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ClickItemContactListener {
     private val rvList: RecyclerView by lazy {
         findViewById<RecyclerView>(R.id.rv_list)
     }
 
-    private val adapter = ContactAdapter()
+    private val adapter = ContactAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
+        fetchListContact()
         bindView()
-        updateList()
+    }
+
+    private fun fetchListContact(){
+        val list = arrayListOf(
+            Contact(
+                "Marcelo Lopes",
+                "(00) 0000-00000",
+                "img.png"
+            ),
+            Contact(
+                "Marcelo Claro",
+                "(99) 9999-99999",
+                "img.png"
+            )
+        )
+       getInstanceSharedPreference().edit {
+           val json = Gson().toJson(list)
+           putString("contacts", json)
+           commit()
+       }
+    }
+
+    private fun getInstanceSharedPreference(): SharedPreferences{
+        return getSharedPreferences("com.mcl.contacts.PREFERENCES", Context.MODE_PRIVATE)
+    }
+
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     private fun bindView(){
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
+        updateList()
+    }
+
+    private fun getListContacts(): List<Contact>{
+        val list = getInstanceSharedPreference().getString("contacts", "[]")
+        val turnsType = object : TypeToken<List<Contact>>(){}.type
+        return Gson().fromJson(list, turnsType)
     }
 
     private fun updateList(){
-        adapter.updateList(
-            arrayListOf(
-                Contact(
-                    "Marcelo Lopes",
-                    "(00) 0000-00000",
-                    "img.png"
-                ),
-                Contact(
-                    "Marcelo Claro",
-                    "(99) 9999-99999",
-                    "img.png"
-            )
-            )
-        )
+        val list = getListContacts()
+        adapter.updateList(list)
     }
 
     private fun showToast(message: String){
@@ -69,6 +109,12 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun clickItemContact(contact: Contact) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(EXTRA_CONTACT, contact)
+        startActivity(intent)
     }
 
 }
